878a3455af3a3fb8b7673ab90d93f05ac9624407
YagoHOC <108764796+YagoHOC@users.noreply.github.com>
2023-08-22 17:34:52 -0300

    Update python.py
    Agora funciona :)

--- python.py
+++ python.py
@@ -1,8 +1,12 @@
-import math
+# ax2 + bx + c = 0
 
-valorA = input('Qual o valor A?')
-valorB = input('Qual o valor B?')
-valorC = input('Qual o valor C?')
-xis = valorB*(-1)+math.sqrt(valorB*valorB - 4*valorA*valorC)
+valorA = int(input('Qual o valor A?'))
+valorB = int(input('Qual o valor B?'))
+valorC = int(input('Qual o valor C?'))
 
-valorA*(xis*xis)+valorB*xis+valorC == 0
+valorX = ((valorB * -1) + (valorB**2 - 4 * valorA * valorC) ** (1/2)) / (2 * valorA)
+valorX2 = ((valorB * -1) + (valorB**2 - 4 * valorA * valorC) ** (1/2)) / (2*valorA)
+
+print('O valor x1 é '+str(valorX))
+print('O valor x2 é '+str(valorX2))
+
